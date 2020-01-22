d204ea3ee76452b941fdba4e2da1c56f2a42fd58
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-01-22 15:03:08 -0800

    Assignment 2
    Add a database called "Project1" to PostgreSQL. 
    Configure the Django settings to add "PythonClub" to the app list and to make PostgreSQL with Project1 your default database. 
    Edit the URL in the Project1 folder to import the URLS from the club folder. 
    Add a urls.py file to the PythonClub folder. 
    Migrate the database.

--- PythonClub/PythonClub/settings.py
+++ PythonClub/PythonClub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Project1' #the parent folder
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'PythonClub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Project1',
+        'USER' : 'postgres',
+        'PASSWORD': 'pi$angkuning60',
+        'HOST' :'localhost',
+        'PORT' :'',
     }
 }
 
