c180487ce4fcad1fcd9c8c7152e3b7b53b59aadb
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-02-05 15:30:25 -0800

    Add files via upload

--- PythonClub/Project1/urls.py
+++ PythonClub/Project1/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('gettypes/', views.gettypes, name='types'),
+    path('getmeetings/', views.getmeetings, name='meetings'),
+    path('getresources/', views.getresources, name='resources'),
+    path('getevents/', views.getevents, name='events'),
+    path('eventsdetails/<int:id>', views.eventsdetails, name='eventdetails'),
 ]
--- PythonClub/Project1/views.py
+++ PythonClub/Project1/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'Project1/index.html')
 
-def gettypes(request):
+def getmeetings(request):
     type_list=Meeting.objects.all()
-    return render(request, 'Project1/types.html' ,{'type_list' : type_list})
+    return render(request, 'Project1/meetings.html' ,{'type_list' : type_list})
+
+def getresources(request):
+    type_list=Resource.objects.all()
+    return render(request, 'Project1/resources.html' ,{'type_list' : type_list})
+
+def getevents(request):
+    type_list=Event.objects.all()
+    return render(request, 'Project1/events.html', {'type_list' : type_list})
+
+def eventsdetails(request, id):
+    event=get_object_or_404(Event, pk=id)
+    
+    return render(request, 'Project1/eventdetails.html', {'event' : event}
+    )
